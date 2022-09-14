6159b531f2655d8745e1513b6cb405dac5a8121b
Fauzan Mursalin <zuchairul@gmail.com>
2022-09-14 15:33:05 +0800

    update home

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,3 +1,6 @@
 <h2>Welcome To Homepage</h2>
-<a href="/todolist/Todo">Todo</a>
+<a href="/todolist/Todo">Tugas 1</a>
+<br><br>
+<a href="/todolist/Biodata">Tugas 2</a>
+<br><br>
 <a href="/todolist/About">About</a>
